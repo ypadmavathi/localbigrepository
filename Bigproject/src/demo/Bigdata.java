1bd3d5ef00d3da327e39248a7370c747834e662d
padmavathi.y <padmavathi.y@DS-9121AA344592.HCLT.CORP.HCL.IN>
2017-08-28 16:08:44 +0530

    deleted output statement

---
+++ Bigproject/src/demo/Bigdata.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class Bigdata extends Smalldata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Smalldata sd=new Smalldata();
+		displayBigdata();
+		sd.display();
+		
+
+	}
+
+	private static void displayBigdata() {
+		System.out.println("Hi");
+		System.out.println("HELLO");
+		
+	}
+
+}
